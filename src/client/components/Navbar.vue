8801e9384c457589ab0f5bc6ba099ce2da7f7638
Андрей Кондратенко <kondratenko_av@astral.ru>
2021-03-06 11:14:43 +0300

    Добавить navbar

---
+++ src/client/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-navbar toggleable="lg" type="light" variant="faded">
+    <div class="container">
+      <b-navbar-brand href="#">
+        <img
+          src="/logo_with_text.svg"
+          class="d-inline-block align-top"
+          alt="alefdoc"
+          height="32"
+        />
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
+  </b-navbar>
+</template>
--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -2,27 +2,20 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
-        alef
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">alef</h1>
+      <!-- prettier-ignore -->
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, ab. Doloribus voluptatum consectetur a! Dignissimos laboriosam maxime alias nobis sit amet omnis fuga, natus doloremque eligendi ratione adipisci voluptatum delectus.
+        Totam laudantium quae quod numquam eum soluta nulla, eligendi quibusdam? Quidem, ipsum iste rerum nesciunt error aliquam nemo impedit nisi ex itaque veritatis, possimus natus perferendis dolorum porro, aspernatur quisquam?
+        Unde, aperiam culpa qui esse eum voluptate laudantium impedit odit itaque eius maxime. Incidunt aut vitae magni saepe inventore repellendus soluta quisquam, rem veritatis atque ea molestiae aspernatur. Ipsa, facilis.
+        Molestias et alias optio quia eaque voluptates inventore illum dolores! Similique tenetur, nulla sint enim animi accusantium perspiciatis adipisci fuga saepe voluptatibus doloribus deserunt deleniti odio eos non repudiandae? Accusantium?
+        Quibusdam, tempore? Voluptate fugiat dicta nam aperiam obcaecati molestiae reiciendis est sit quas fuga, totam inventore nihil sequi officiis nulla quidem unde. Accusantium, temporibus natus inventore necessitatibus vel maxime sunt.
+        Distinctio temporibus ut ab sint eius? Libero nobis voluptatibus dolorem eum, labore neque tempora cupiditate earum quidem, molestiae ipsam voluptatum necessitatibus mollitia nihil, asperiores culpa alias cum eligendi eaque. Ad.
+        Aperiam rerum rem, sint voluptatem exercitationem cupiditate delectus quisquam totam voluptate porro accusantium. Provident animi quae numquam repudiandae id non est maiores quod. Doloremque beatae ipsum quod adipisci. Magnam, aperiam?
+        Cupiditate ducimus mollitia sit non repellendus nulla sed sapiente iste reiciendis commodi, illo qui ratione, ex velit tenetur autem error eius repudiandae? Sapiente, saepe molestias culpa expedita dignissimos unde similique.
+        Quia nulla aliquid ducimus repellendus tenetur porro soluta, est harum atque optio molestias maxime facilis incidunt corrupti excepturi odit magni ad quidem ullam eligendi sit quas corporis. At, ipsam dolorem?
+        Fugit, sint incidunt adipisci, distinctio saepe eius ipsam fuga quam dolor vitae eos, molestiae unde omnis sapiente autem velit maiores et consequuntur ex consequatur necessitatibus reiciendis officia. Enim, similique et.
+      </p>
     </div>
   </div>
 </template>
@@ -34,7 +27,6 @@ export default {}
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -42,16 +34,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
