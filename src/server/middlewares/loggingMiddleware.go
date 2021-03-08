a52d11d4fb06d5af6b34f82ab44ed35014d93d25
Андрей Кондратенко <264794@gmail.com>
2021-03-09 00:02:37 +0300

    Пока убрать тело запроса из логов

--- src/server/middlewares/loggingMiddleware.go
+++ src/server/middlewares/loggingMiddleware.go
@@ -7,7 +7,7 @@ import (
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.RequestURI, r.Body)
+		log.Println(r.Method, r.RequestURI)
 		next.ServeHTTP(w, r)
 	})
 }
