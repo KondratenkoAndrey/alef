42ce7a03f7b8958290bf627ab578a23b43587ce7
Андрей Кондратенко <kondratenko_av@astral.ru>
2021-03-08 23:49:01 +0300

    Вынести логи в middleware

--- src/server/controllers/companyController.go
+++ src/server/controllers/companyController.go
@@ -3,12 +3,10 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/KondratenkoAndrey/alef/models"
-	"log"
 	"net/http"
 )
 
 func GetCompanyInfo(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.Method, r.URL)
 	w.Header().Set("Content-Type", "application/json")
 	pages := models.GetCompanyInfo()
 	json.NewEncoder(w).Encode(pages)
--- src/server/main.go
+++ src/server/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"github.com/KondratenkoAndrey/alef/controllers"
+	"github.com/KondratenkoAndrey/alef/middlewares"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/company-info", controllers.GetCompanyInfo)
+	env := os.Getenv("APP_ENV")
+	if strings.ToLower(env) != "production" {
+		router.Use(middlewares.LoggingMiddleware)
+	}
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
